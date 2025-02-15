902c5f4cb30d98615a00983e92209ad222a920b6
apehex <apehex@protonmail.com>
2025-02-15 22:41:50 +0100

    Fix names

--- scrapscii/settings.py
+++ scrapscii/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for scrascii project
+# Scrapy settings for scrapscii project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = "scrascii"
+BOT_NAME = "scrapscii"
 
-SPIDER_MODULES = ["scrascii.spiders"]
-NEWSPIDER_MODULE = "scrascii.spiders"
+SPIDER_MODULES = ["scrapscii.spiders"]
+NEWSPIDER_MODULE = "scrapscii.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "scrascii (+http://www.yourdomain.com)"
+#USER_AGENT = "scrapscii (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    "scrascii.middlewares.ScrasciiSpiderMiddleware": 543,
+#    "scrapscii.middlewares.ScrapsciiSpiderMiddleware": 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    "scrascii.middlewares.ScrasciiDownloaderMiddleware": 543,
+#    "scrapscii.middlewares.ScrapsciiDownloaderMiddleware": 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    "scrascii.pipelines.ScrasciiPipeline": 300,
+#    "scrapscii.pipelines.ScrapsciiPipeline": 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
