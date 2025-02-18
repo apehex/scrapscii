0e65589e6fe427f8215c93062f2b2be9c057b33c
apehex <apehex@protonmail.com>
2025-02-18 12:26:26 +0100

    Fetch the labels + strip the caption + tidy

--- scrapscii/spiders/twitchquotes_spider.py
+++ scrapscii/spiders/twitchquotes_spider.py
@@ -26,10 +26,12 @@ class TwitchQuotesSpider(scrapy.Spider):
             # parse
             __caption = __pasta.css('h3.-title-inner-parent::text').get()
             __content = __pasta.css('span.-main-text::text').get()
+            __labels = __pasta.css('h4.tag-label::text').getall()
             # format
             if __caption and __content:
                 yield {
-                    'caption': __caption,
+                    'caption': __caption.strip(),
                     'content': __content,
+                    'labels': ','.join(__l.strip().capitalize() for __l in set(__labels) if __l.strip()),
                     'charsets': ','.join(set(scrapscii.unicode.lookup_section(__c) for __c in __content)),
                     'chartypes': ','.join(set(scrapscii.unicode.lookup_category(__c) for __c in __content)),}
