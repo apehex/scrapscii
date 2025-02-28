80a05619abf51abd57e3eb4015bc89956f86e5ab
apehex <apehex@protonmail.com>
2025-02-28 17:37:53 +0100

    Tidy up: split into functions for readability

--- scripts/stylize.py
+++ scripts/stylize.py
@@ -21,8 +21,9 @@ import scrapscii.unicode
 
 WIDTH_MIN = 16
 WIDTH_MAX = 128
-SHARD_LEN = 2**12 # min size of a dataset shard
-TOTAL_LEN = 2**15
+TABLE_LEN = 2**5
+SHARD_LEN = 2**10
+TOTAL_LEN = 2**12
 
 # IO ###########################################################################
 
@@ -46,62 +47,120 @@ def is_valid_ascii(ascii: str, width: int=WIDTH_MIN) -> bool:
         and len(ascii) >= width
         and not 'error: can\'t decode' in ascii.lower())
 
-# EXTRACT ######################################################################
+# DOWNLOAD #####################################################################
 
-if __name__ == '__main__':
-    # shard index
-    __shard = 0
+def download_image(url: str, timeout: int=1) -> bytes:
+    __bytes = b''
+    # retrieve the image content as bytes
+    try:
+        __response = requests.get(url, timeout=timeout)
+        __bytes = __response.content
+    # ignore exceptions
+    except Exception as __e:
+        __bytes = b''
+    # default
+    return __bytes
 
-    # download the dataset
-    __table = []
-    __dataset = datasets.load_dataset('apple/DataCompDR-12M', split='train', cache_dir='~/.cache/huggingface/datasets', streaming=True)
-    __iter = itertools.islice(__dataset, 0, TOTAL_LEN)
+def format_path(url: str, temp: str=TEMP_PATH) -> str:
+    # parse the URL
+    __path = urllib.parse.urlparse(url).path
+    __filename = __path.split('/')[-1]
+    __extension = os.path.splitext(__filename)[-1]
+    # reduce the filename to a fixed size
+    __hash = hashlib.sha1(url.encode('utf-8')).hexdigest()
+    # safe path
+    return os.path.join(temp, __hash + __extension)
+
+def export_image(data: bytes, path: str) -> None:
+    with open(path, 'b+w') as __file:
+        __file.write(data)
+
+# CLEAR ########################################################################
+
+def list_files(path: str, extension: str='') -> list:
+    return [
+        os.path.join(__dp, __f)
+        for __dp, __dn, __fn in os.walk(path)
+        for __f in __fn if extension in __f]
+
+def clear_dir(path: str) -> None:
+    __paths = list_files(path)
+    for __p in __paths:
+        os.remove(__p)
+
+# RANDOM #######################################################################
+
+def random_options(width_min: int=WIDTH_MIN, width_max: int=WIDTH_MAX) -> list:
+    # choose the config randomly
+    __width = '--width {width}'.format(width=random.randint(width_min, width_max))
+    __braille = '--braille' if random.choice([True, False]) else ''
+    __complex = '--complex' if random.choice([True, False]) else ''
+    __dither = '--dither' if __braille and random.choice([True, False]) else ''
+    __grayscale = '--grayscale' if random.choice([False]) else '' # colorless terminal
+    __negative = '--negative' if random.choice([True, False]) else ''
+    # chain all the options
+    return [__width, __braille, __complex, __dither, __grayscale, __negative]
+
+def format_args(options: list) -> list:
+    return list(itertools.chain.from_iterable(__o.split(' ') for __o in options if __o))
+
+def format_labels(options: list) -> list:
+    return list(itertools.chain.from_iterable(__o.strip('--') for __o in options if __o))
+
+# EXPORT #######################################################################
+
+def export_table(table: iter, index: int, path: str=DATA_PATH) -> None:
+    __path = os.path.join(path, '{index:0>4d}.parquet'.format(index=index))
+    scrapscii.data.export_table_as_parquet(table=table, path=__path)
+
+# CONVERT ######################################################################
+
+def convert_shard(
+    dataset: iter,
+    table: iter=[],
+    table_idx: int=0,
+    table_len: int=TABLE_LEN,
+    shard_len: int=SHARD_LEN,
+    width_min: int=WIDTH_MIN,
+    width_max: int=WIDTH_MAX,
+    temp_path: str=TEMP_PATH,
+    data_path: str=DATA_PATH,
+) -> tuple:
+    # current table
+    __index = table_idx # index
+    __table = list(table) # data
+
+    # take a shard's worth of data
+    __iter = itertools.islice(dataset, 0, shard_len)
 
     # iterate over the samples
-    for __sample in tqdm.tqdm(__iter, total=TOTAL_LEN):
+    for __sample in tqdm.tqdm(__iter, total=shard_len):
 
         # parse the URL
         __url = __sample['url.txt']
-        __hash = hashlib.sha1(__url.encode('utf-8')).hexdigest()
-        __path = urllib.parse.urlparse(__url).path
-        __filename = __path.split('/')[-1]
-        __extension = os.path.splitext(__filename)[-1]
+        __path = format_path(url=__url, temp=temp_path)
 
         # download image from URL
-        try:
-            __response = requests.get(__url, timeout=1)
-        except:
-            tqdm.tqdm.write(f'Failed to download {__url}')
-            continue
+        __bytes = download_image(__url)
 
-        # save image on disk
-        __path = os.path.join(TEMP_PATH, __hash + __extension)
-        __bytes = __response.content
+        # check hex digest
         if is_valid_image(__bytes):
-            with open(__path, 'b+w') as __file:
-                __file.write(__bytes)
+            export_image(data=__bytes, path=__path)
         else:
             tqdm.tqdm.write(f'Skip corrupted {__url}')
             continue
 
         # choose the config randomly
-        __width = '--width {width}'.format(width=random.randint(WIDTH_MIN, WIDTH_MAX))
-        __braille = '--braille' if random.choice([True, False]) else ''
-        __complex = '--complex' if random.choice([True, False]) else ''
-        __dither = '--dither' if __braille and random.choice([True, False]) else ''
-        __grayscale = '--grayscale' if random.choice([False]) else '' # colorless terminal
-        __negative = '--negative' if random.choice([True, False]) else ''
+        __options = random_options(width_min=width_min, width_max=width_max)
+        __args = format_args(__options)
+        __labels = format_labels(__options)
 
         # choose a caption among the synthetic text
         __choice = random.randint(0, len(__sample['syn.json']['syn_text']) - 1)
         __caption = __sample['syn.json']['syn_text'][__choice]
 
-        # export the conversion config
-        __labels = [__l for __l in [__width, __braille, __complex, __dither, __grayscale, __negative] if __l]
-
         # convert the image to ASCII art
-        __flags = list(itertools.chain.from_iterable(__l.split(' ').strip('--') for __l in __labels if __l))
-        __process = subprocess.run(['ascii-image-converter'] + __flags + [__path], stdout=subprocess.PIPE)
+        __process = subprocess.run(['ascii-image-converter'] + __args + [__path], stdout=subprocess.PIPE)
         __content = __process.stdout.decode('utf-8')
 
         # check for conversion errors
@@ -117,21 +176,39 @@ if __name__ == '__main__':
             continue
 
         # chunk the dataset into shards
-        if len(__table) >= SHARD_LEN:
+        if len(__table) >= table_len:
             # export as parquet
-            pq.write_table(
-                table=pl.Table.from_pylist(
-                    mapping=__table,
-                    schema=scrapscii.data.SCHEMA),
-                where=os.path.join(DATA_PATH, '{shard:0>4d}.parquet'.format(shard=__shard)))
+            export_table(table=__table, index=__index, path=data_path)
             # refresh
-            __shard += 1
+            __index += 1
             __table = []
 
-    # export the remainder
-    if len(__table) > 0:
-        pq.write_table(
-            table=pl.Table.from_pylist(
-                mapping=__table,
-                schema=scrapscii.data.SCHEMA),
-            where=os.path.join(DATA_PATH, '{shard:0>4d}.parquet'.format(shard=__shard)))
+    # return the remainder
+    return (__index, __table)
+
+# MAIN #########################################################################
+
+if __name__ == '__main__':
+    # init the table
+    __index = 0 # index
+    __table = [] # data
+
+    # init the dataset
+    __dataset = datasets.load_dataset('apple/DataCompDR-12M', split='train', cache_dir='~/.cache/huggingface/datasets', streaming=True)
+    __iter = itertools.islice(__dataset, 0, TOTAL_LEN)
+
+    # convert shard by shard
+    while __iter:
+        # export a shard
+        __index, __table = convert_shard(
+            dataset=__iter,
+            table=__table,
+            table_idx=__index,
+            table_len=TABLE_LEN,
+            shard_len=SHARD_LEN,
+            width_min=WIDTH_MIN,
+            width_max=WIDTH_MAX,
+            temp_path=TEMP_PATH,
+            data_path=DATA_PATH,)
+        # remove the temp downloads (images)
+        clear_dir(TEMP_PATH)
